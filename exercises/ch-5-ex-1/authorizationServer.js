e2ae7e196a70a4897b733f99563d40dede0e3455
hongil hwang <hongilhwang@gmail.com>
2020-12-15 02:40:05 +0900

    인가서버로가 액세스 토큰을 발급 하기 까지의 처리

--- exercises/ch-5-ex-1/authorizationServer.js
+++ exercises/ch-5-ex-1/authorizationServer.js
@@ -124,6 +124,78 @@ app.post("/token", function(req, res){
 	 * Process the request, issue an access token
 	 */
 
+	let clientId;
+	let clientSecret;
+
+	const auth = req.headers['authorization'];
+	if(auth){
+		const clientCredentials = decodeClientCredentials(auth);
+		clientId = clientCredentials.id;
+		clientSecret = clientCredentials.secret;
+	}
+
+	if(req.body.client_id){
+		if(clientId){
+			res.status(401).json({error : 'invalid_client'});
+			return;
+		}
+
+		clientId = req.body.client_id;
+		clientSecret = req.body.client_secret;
+	}
+
+	const client = getClient(clientId);
+	if (!client) {
+		console.log('Unknown client %s', clientId);
+		res.status(401).json({error: 'invalid_client'});
+		return;
+	}
+
+	if (client.client_secret != clientSecret) {
+		console.log('Mismatched client secret, expected %s got %s', client.client_secret, clientSecret);
+		res.status(401).json({error: 'invalid_client'});
+		return;
+	}
+
+	if (req.body.grant_type == 'authorization_code') {
+
+		var code = codes[req.body.code];
+
+		if (code) {
+			delete codes[req.body.code]; // burn our code, it's been used
+			if (code.request.client_id == clientId) {
+
+				var access_token = randomstring.generate();
+				nosql.insert({ access_token: access_token, client_id: clientId });
+
+
+				console.log('Issuing access token %s', access_token);
+				console.log('with scope %s', code.scope);
+
+				var token_response = { access_token: access_token, token_type: 'Bearer' };
+
+				res.status(200).json(token_response);
+				console.log('Issued tokens for code %s', req.body.code);
+
+				return;
+			} else {
+				console.log('Client mismatch, expected %s got %s', code.request.client_id, clientId);
+				res.status(400).json({error: 'invalid_grant'});
+				return;
+			}
+
+		} else {
+			console.log('Unknown code, %s', req.body.code);
+			res.status(400).json({error: 'invalid_grant'});
+			return;
+		}
+
+	} else {
+		console.log('Unknown grant type %s', req.body.grant_type);
+		res.status(400).json({error: 'unsupported_grant_type'});
+	}
+
+
 });
 
 var buildUrl = function(base, options, hash) {
